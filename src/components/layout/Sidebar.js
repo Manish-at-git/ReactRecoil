a1d53f8b0cfd66daa7467105e1b095d3aa553dd5
unknown <apit2469@gmail.com>
2022-08-24 17:29:14 +0530

    RECOIL IN REACT

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -15,18 +15,6 @@ function Sidebar() {
       <div className="columns is-multiline">
         <div className="column is-12">
           <h2 className="subtitle">({totalItems}) products</h2>
-          <div className="field has-addons">
-            <div className="control">
-              <button className="button is-dark">
-                <i class="fas fa-th-large"></i>
-              </button>
-            </div>
-            <div className="control">
-              <button className="button">
-                <i class="fas fa-grip-horizontal"></i>
-              </button>
-            </div>
-          </div>
           <h3 className="subtitle is-6 mb-2">Search Products</h3>
           <input
             class="input"
